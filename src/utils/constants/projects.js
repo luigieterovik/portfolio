e51e8df9055551b18c4b7b119e5ff5ca1fad6b87
Luigi  Olivi <eterowiczluigi@gmail.com>
2024-05-01 19:50:38 -0300

    some projects incremented to the document

--- src/utils/constants/projects.js
+++ src/utils/constants/projects.js
@@ -1,9 +1,27 @@
 const projects = [
   {
-    image: "",
+    image: "marisProject.png",
     title: "Mari's Boutik's",
-    description: "Compre perfumes exclusivos de Mari's Boutik's",
+    description:
+      "Mari's Boutik's é o site oficial para compra dos produtos cosméticos fabricados por Mari's Boutik's. O usuário poderá registrar uma conta e realizar compras dos produtos disponíveis. (Em desenvolvimento)",
     tags: ["JavaScript", "React", "Node.js", "API", "Docker", "PostgreSQL"],
+    link: "https://maris-frontend.vercel.app/",
+  },
+  {
+    image: "userProject.png",
+    title: "Create Users",
+    description:
+      "Este projeto acadêmico é aplicação que permite a criação de usuários. Utilizando uma API, o front-end consome os dados, resultando na exibição dos usuários criados. Essa experiência ampliou minha compreensão de desenvolvimento web e integração de sistemas.",
+    tags: ["JavaScript", "React", "Node.js", "API", "Axios"],
+    link: "https://create-users-pi.vercel.app/",
+  },
+  {
+    image: "calculadoraProject.png",
+    title: "Calculadora de Tendência Central e Dispersão",
+    description:
+      "Uma página que calcula medidas de tendência central e dispersão, que desenvolvi durante o ensino médio, como parte das aulas de matemática. Esse projeto não só aprofundou meu entendimento da disciplina, mas também me proporcionou uma oportunidade única: fui convidado para palestrar sobre tecnologia e programação para um público de cerca de 100 pessoas.",
+    tags: ["JavaScript", "React", "Node.js", "API", "Docker", "PostgreSQL"],
+    link: "https://luigieterovik.github.io/medidas-tendencia-central-e-dispersao/",
   },
 ];
 
